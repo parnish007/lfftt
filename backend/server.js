754d9a2286d9309060656c080d3dc637fab60d0e
parnish007 <parnishklpo@gmail.com>
2025-07-05 18:33:32 +0545

    6 33 update

--- backend/server.js
+++ backend/server.js
@@ -83,6 +83,14 @@ app.use('/api/bills', require('./routes/bills'));
 // ✅ WebSocket
 require('./socket')(io);
 
+// ✅ Serve robots.txt manually to ensure Google gets correct file
+app.get('/robots.txt', (req, res) => {
+  res.type('text/plain');
+  res.send(`User-agent: *
+Allow: /
+Sitemap: https://lifeforfuntours.com/sitemap.xml`);
+});
+
 // ✅ Serve index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/html/index.html'));
